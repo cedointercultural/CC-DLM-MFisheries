2440e7e50c0e90f5f6cf341d249cca1ce739b05b
Ricardo Cavieses <rcavieses@gmail.com>
2024-10-24 10:29:01 -0700

    modified:   Forecast_one_species copy 3.ipynb 	modified:   Forecast_one_species.ipynb 	new file:   SHAP2.ipynb 	new file:   data/data/data_merge.py 	new file:   data/futuretemp1/merge_temp.py 	new file:   model_explain.ipynb 	new file:   model_opener.ipynb 	modified:   tabla_resultados.ipynb

---
+++ data/data/data_merge.py
@@ -0,0 +1,26 @@
+import os
+import pandas as pd
+
+# Especifica la carpeta que contiene los archivos
+folder_path = 'futuretemp'
+
+# Lista para almacenar los DataFrames
+dataframes = []
+
+# Listar todos los archivos en la carpeta
+for file_name in os.listdir(folder_path):
+    # Crear la ruta completa del archivo
+    file_path = os.path.join(folder_path, file_name)
+    # Verificar si el archivo es un archivo CSV
+    if file_name.endswith('.csv'):
+        # Leer el archivo CSV y agregarlo a la lista de DataFrames
+        df = pd.read_csv(file_path)
+        dataframes.append(df)
+
+# Concatenar todos los DataFrames en uno solo
+merged_df = pd.concat(dataframes, ignore_index=True)
+
+# Guardar el DataFrame resultante en un archivo CSV llamado data.csv
+merged_df.to_csv('future_data.csv', index=False)
+
+print("Archivos combinados y guardados en 'data.csv'.")
